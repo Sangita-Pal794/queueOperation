a5f1518bfdec4df61b54f17f55eec4e33defeca7
Sangita <pal07sangita@gmail.com>
2020-11-21 12:07:31 +0530

    Refactored:Added function to take user choice

--- Queue.js
+++ Queue.js
@@ -3,18 +3,18 @@ let readlineSync = require('readline-sync');
 
 /*taking user input*/
 Choice = function () {
-    let isTerminated = true;
-    while (isTerminated == true) {
+    let flag = true;
+    while (flag == true) {
         let ch = readlineSync.question("\nEner your choice:\n1.Enqueue\n2.Dequeue\n3.displayQueue\n4.Exit\n");
         switch (ch) {
-            case '1': let userInput = readlineSync.question("Enter item to be push:");
+            case '1': let userInput = readlineSync.question("Enter item to be pushed:");
                 util.enque(userInput);
                 break;
             case '2': util.dequeue();
                 break;
             case '3': util.displayQueue();
                 break;    
-            case '4': isTerminated = false;
+            case '4': flag = false;
                 break;
             default: console.log("Invalid choice!!Please enter valid choice");
         }
